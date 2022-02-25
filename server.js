e5150eeaccbde4b59bbe5ccc2b6211153c30dce7
Rory-McDermid <60363659+Rory-McDermid@users.noreply.github.com>
2022-02-25 16:53:49 -0600

    move settings to config

--- server.js
+++ server.js
@@ -2,18 +2,14 @@ const http = require('http');
 const fs = require('fs');
 const url = require('url');
 
-const hostname = '127.0.0.1';
-const port = 3000;
+var config = require('./config.json');
 
 const readingTypes = ['noiseReading', 'motionReading'];
 
 const {InfluxDB, Point} = require('@influxdata/influxdb-client')
-const token = "8MkGu8tDM4AHKB69seAD_2mkaOxYlO7CR0xVP-UcQIkI2GnWJwiZ1TkLEq9nrHmjbU4Rj_BmsfarBqenAcjO7w=="
-const org = 'rmm180000@utdallas.edu'
-const bucket = 'mock_I'
-const client = new InfluxDB({url: 'https://us-east-1-1.aws.cloud2.influxdata.com', token: token})
+const client = new InfluxDB({url: config.influxDB.url, token: config.influxDB.token})
 
-queryApi = client.getQueryApi(org);
+queryApi = client.getQueryApi(config.influxDB.org);
 
 function sendFile(fileLoc, res){
 	const stream = fs.createReadStream(fileLoc);
@@ -26,7 +22,7 @@ function sendFile(fileLoc, res){
 }
 
 function apiReq(path, res){
-	u = new URL(path, 'http://localhost');
+	u = new URL(path, 'http://${config.server.hostname}');
 	if(u.pathname == '/api/recent')apiRecent(res);
 	else{
 		res.setHeader('Content-Type', 'text/plain');
@@ -37,13 +33,11 @@ function apiReq(path, res){
 function apiRecent(res){
 	let obj = {//test object
 		noiseReading : [
-			//{sensor : '1', time : 0, value: 42.24}
 		],
 		motionReading : [
-			//{sensor : '1', time : 0, value: 42.24}
 		]};
 	const q =
-		'from(bucket: "mock_II")\
+		'from(bucket: "'+config.influxDB.bucket+'")\
 		|> range(start: 0)\
 		|> last()';
 	const observer = {
@@ -62,8 +56,6 @@ function apiRecent(res){
 		}
 	};
 	queryApi.queryRows(q, observer);
-	//res.setHeader('Content-Type', 'aplication/json');
-	//res.end("err");
 }
 
 function apiSince(u, res){
@@ -76,11 +68,9 @@ const server = http.createServer((req, res) => {
 	else if(req.url.startsWith('/api/'))apiReq(req.url, res);
 	else{
 		sendFile('static' + req.url, res);
-		//res.setHeader('Content-Type', 'text/plain');
-		//res.end('Hello World');
 	}
 });
 
-server.listen(port, hostname, () => {
-	console.log(`Server running at http://${hostname}:${port}/`);
+server.listen(config.server.port, config.server.hostname, () => {
+	console.log(`Server running at http://${config.server.hostname}:${config.server.port}/`);
 });
