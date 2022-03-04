56a7f9ebb509ab8acb793ee3d5166838b103fbe2
Rory-McDermid <60363659+Rory-McDermid@users.noreply.github.com>
2022-03-04 15:30:05 -0600

    Add comments

--- server.js
+++ server.js
@@ -2,15 +2,16 @@ const http = require('http');
 const fs = require('fs');
 const url = require('url');
 
-var config = require('./config.json');
+var config = require('./config.json'); //load configuration file
 
-const readingTypes = ['noiseReading', 'motionReading'];
+//const readingTypes = ['noiseReading', 'motionReading'];
 
 const {InfluxDB, Point} = require('@influxdata/influxdb-client')
 const client = new InfluxDB({url: config.influxDB.url, token: config.influxDB.token})
 
 queryApi = client.getQueryApi(config.influxDB.org);
 
+//send file to client, used for static content
 function sendFile(fileLoc, res){
 	const stream = fs.createReadStream(fileLoc);
 	stream.on('error', (err) => {
@@ -21,12 +22,15 @@ function sendFile(fileLoc, res){
 	stream.pipe(res);
 }
 
+//called too send error response to client
 function send400err(str, res){
 	res.setHeader('Content-Type', 'text/plain');
 	res.writeHead(400, 'error');
 	res.end(str);
 }
 
+//several endpoints use the same observer, so we have a function to construct
+//it to remove redundancy.
 function makeObserver(res, obj){
 	const observer = {
 		next(row, tableMeta) {
@@ -48,6 +52,7 @@ function makeObserver(res, obj){
 function apiReq(path, res){
 	u = new URL(path, `http://${config.server.hostname}`);
 	//console.log(u);
+	//call function for specific api endpoints
 	if(u.pathname == '/api/recent')apiRecent(res);
 	else if(u.pathname == '/api/since')apiSince(u, res);
 	else if(u.pathname == '/api/noiseReading/average')apiNoiseAverage(u, res);
@@ -57,12 +62,10 @@ function apiReq(path, res){
 }
 
 function apiRecent(res){
-	let obj = {//test object
-		noiseReading : [
-		],
-		motionReading : [
-		]};
-	const q =
+	let obj = {
+		noiseReading : [],
+		motionReading : []};
+	const q = //querys are writen in flux, a language created for influxDB
 		`from(bucket: "${config.influxDB.bucket}")
 		|> range(start: 0)
 		|> last()`;
@@ -74,7 +77,7 @@ function apiSince(u, res){
 	/********************************************
 	NOTE: the way this query is being constructed
 	is NOT SECURE. The inputs are not verified &
-	are vunrable to a code injection attack.
+	are vulnerable to a code injection attack.
 	********************************************/
 	let obj;
 	let q = 
@@ -106,7 +109,7 @@ function apiNoiseAverage(u, res){
 	/********************************************
 	NOTE: the way this query is being constructed
 	is NOT SECURE. The inputs are not verified &
-	are vunrable to a code injection attack.
+	are vulnerable to a code injection attack.
 	********************************************/
 	let q =
 		`from(bucket: "${config.influxDB.bucket}")
@@ -134,6 +137,7 @@ function apiNoiseAverage(u, res){
 	queryApi.queryRows(q, observer);
 }
 
+//create server, for each request call either sendfile(filename, res) or apiReq(url, res)
 const server = http.createServer((req, res) => {
 	res.statusCode = 200;
 	if(req.url == '/')sendFile('index.html', res);
